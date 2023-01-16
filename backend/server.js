18f54d1e9622a783e0331954ff82f8d843072995
amejid <abdelmejid89@gmail.com>
2023-01-16 16:43:57 +0300

    Configure DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config()
 import products from './data/products.js';
+import connectDB from "./config/db.js";
+
+connectDB();
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -19,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
+app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
