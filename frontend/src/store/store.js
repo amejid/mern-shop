6768bcb02527dc82fbd56aab00dc69eb5ac9569f
amejid <abdelmejid89@gmail.com>
2023-01-18 21:23:14 +0300

    Setup registration action and reducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,13 +1,14 @@
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import {productDetailsReducer, productListReducer} from "./reducers/productReducers";
 import {cartReducer} from "./reducers/cartReducers";
-import {userLoginReducer} from "./reducers/userReducers";
+import {userLoginReducer, userRegisterReducer} from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
