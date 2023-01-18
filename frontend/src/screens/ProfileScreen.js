159861bc966948b1b1375f2d16cfeccd3cd2934a
amejid <abdelmejid89@gmail.com>
2023-01-18 22:31:18 +0300

    Finalize profile update functionality

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,93 @@
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Button, Col, Form, Row} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import {getUserDetails, updateUserProfile} from "../store/actions/userActions";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import {USER_UPDATE_PROFILE_RESET} from "../store/constants/userConstants";
+
+const ProfileScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userDetail = useSelector(state => state.userDetail);
+  const {loading, error, user} = userDetail;
+
+  const userLogin = useSelector(state => state.userLogin);
+  const {userInfo} = userLogin;
+
+  const userUpdateProfile = useSelector(state => state.userUpdateProfile);
+  const {success} = userUpdateProfile;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/login')
+    } else {
+      if (!user || !user.name || success) {
+        dispatch({type: USER_UPDATE_PROFILE_RESET});
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [navigate, userInfo, user, dispatch, success])
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      dispatch(updateUserProfile({id: user._id, name, email, password}));
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {message && <Message variant="danger">{message}</Message>}
+        {error && <Message variant="danger">{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
+        {loading && <Loader/>}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="py-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" value={name}
+                          onChange={(e) => setName(e.target.value)}></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email" className="py-2">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={email}
+                          onChange={(e) => setEmail(e.target.value)}></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password" className="py-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" value={password}
+                          onChange={(e) => setPassword(e.target.value)}></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="py-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Confirm password" value={confirmPassword}
+                          onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+          </Form.Group>
+
+          <Button type="submit" variant="primary" className="my-3">Update</Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
