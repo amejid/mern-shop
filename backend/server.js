0230c88dff943359ec0c8c35ee06a63b7c0ddca0
amejid <abdelmejid89@gmail.com>
2023-01-18 19:08:50 +0300

    Implement user registration and login

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,20 @@ dotenv.config()
 import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
 import {errorHandler, notFound} from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 })
 
 app.use('/api/products', productRoutes);
+
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
