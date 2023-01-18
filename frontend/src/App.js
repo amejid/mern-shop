8823692b437c51eb149345850c73bc56c9259dc5
amejid <abdelmejid89@gmail.com>
2023-01-18 21:03:38 +0300

    Create login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 import {Route, Routes} from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="/" element={<HomeScreen/>}/>
             <Route path="/product/:id" element={<ProductScreen/>}/>
             <Route path="/cart/:id?" element={<CartScreen/>}/>
+            <Route path="/login" element={<LoginScreen/>}/>
           </Routes>
         </Container>
       </main>
