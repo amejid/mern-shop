60393816db716ddebeb43577c5023a1f746a6cf7
amejid <abdelmejid89@gmail.com>
2023-01-24 21:40:36 +0300

    Setup a route to delete a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
-
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
@@ -17,6 +16,20 @@ export const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// @access Private/Admin
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove()
+    res.json({message: 'Product removed'})
   } else {
     res.status(404)
     throw new Error('Product not found')
